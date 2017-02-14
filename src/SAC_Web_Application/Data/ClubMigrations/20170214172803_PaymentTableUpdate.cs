9697527472e866cb15a51e79a88753916823d09e
PaulLittle <S00156762@mail.itsligo.ie>
2017-02-14 17:30:09 +0000

    PaymentsUpdate & DB Write

---
+++ src/SAC_Web_Application/Data/ClubMigrations/20170214172803_PaymentTableUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SAC_Web_Application.Data.ClubMigrations
+{
+    public partial class PaymentTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "Payments",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreateTime",
+                table: "Payments",
+                nullable: true);
+        }
+    }
+}
