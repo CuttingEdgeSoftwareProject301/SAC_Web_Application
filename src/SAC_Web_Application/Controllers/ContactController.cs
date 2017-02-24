3cb5e4332bbbc9b77dba76687024f3dee1c287ab
David O Grady <rad301s00152497@outlook.com>
2017-02-24 22:10:03 +0000

    Contact Us form working using sendgrid

---
+++ src/SAC_Web_Application/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SAC_Web_Application.Models.ContactViewModels;
+using SAC_Web_Application.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SAC_Web_Application.Controllers
+{
+    [RequireHttps]
+    [Authorize]
+    public class ContactController : Controller
+    {
+        private IEmailSender _emailSender;
+
+        public ContactController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+            
+        }
+
+        // GET: Events/Create
+        public IActionResult ContactUs(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        // POST: Contact/Send Question
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SendQuestion(ContactUsViewModel contactUs, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                _emailSender.ContactUsEmailAsync(contactUs.Email, "paull1068@gmail.com", contactUs.Question
+                   ,contactUs.FullName, contactUs.Subject);
+                ModelState.AddModelError(string.Empty, "Your message has been received, we will be in touch very soon");
+                return View("ContactUs", contactUs);
+
+            }
+            return View(contactUs);
+        }
+    }
+}
